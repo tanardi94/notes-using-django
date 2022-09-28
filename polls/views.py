7fac6679c7d1fa09fd6da233f6e2c0e7f760f972
Ardi <tan.ardi94@gmail.com>
2022-09-29 00:50:15 +0700

    adding choice and question detail

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
 
 # Create your views here.
 def index(request):
-    question = Question.objects.get(id=1)
-    return HttpResponse(question.question_text)
+    latest_question_list = Question.objects.order_by('-pub_date')[:4]
+
+    data = {
+        'latest_question_list': latest_question_list
+    }
+
+    return render(request, 'polls/index.html', data)
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
-    return HttpResponse("You're looking at question: %s" % question.question_text)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
 
 def results(request, question_id):
     return HttpResponse("You're looking for results for question %s" % question_id)
